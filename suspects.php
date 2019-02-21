23fb7ac9ecdf1a5248f71ec4e5f1e2126d533761
joy <joy7kibe@gmail.com>
2019-02-21 12:18:39 +0300

    police App v1

--- suspects.php
+++ suspects.php
@@ -19,5 +19,50 @@ require 'security.php';
 </head>
 <body>
 <?php require 'navbar.php'; ?>
+<div class="container">
+    <table class="table">
+        <thead>
+        <tr>
+        <td>ID</td>
+        <td>NAMES</td>
+        <td>IDENTITY</td>
+        <td>GENDER</td>
+        <td>DATE</td>
+        <td>TYPE</td>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+            <td>i</td>
+            <td>John Mark</td>
+            <td>675845</td>
+            <td>Male</td>
+            <td>2019-02-10</td>
+            <td>Murder</td>
+        </tr>
+<?php
+require 'db.php';
+$sql = "select * from suspects";
+$results = mysqli_query($conn,$sql);
+ while ($row = mysqli_fetch_assoc($results))
+ {
+     extract($row);
+     echo" <tr>
+            <td>$id</td>
+            <td>$names</td>
+            <td>$identity</td>
+            <td>$gender</td>
+            <td>$date</td>
+            <td>$type</td>
+        </tr>";
+ }
+?>
+
+        </tbody>
+    </table>
+</div>
+
+
 </body>
 </html>
